4647712c7d7dc9b4f2a8eb0c3d27c230681ade44
Viego-CW <viegogwen59@gmail.com>
2026-01-22 04:26:31 +0700

    Update module part 8

---
+++ MegaMall/Migrations/20260121200004_AddCancelAndReturnReasonToOrder.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MegaMall.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelAndReturnReasonToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CancelReason",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReturnReason",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CancelReason",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ReturnReason",
+                table: "Orders");
+        }
+    }
+}
